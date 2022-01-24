07179d8ef7ebd0b49d3196c5a454f8262ac3f56d
salvosanzone <salvosansone.ss@gmail.com>
2022-01-24 15:47:35 +0100

    creazione header

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="{{asset('css/app.css')}}">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0-9/css/all.min.css" integrity="sha512-X8jR8oYnEmXhGB43u+qxt0mQezR37NUxHhtukvD4bzeWDthOz0mvNOYOzPCMZ7VB0mG607nrkd+kakmSmbffyQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <title>Document</title>
+</head>
+<body>
+
+  @include('partials.header')
+
+  @yield('content')
+
+
+</body>
+</html>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,36 @@
+<header class="d-flex align-items-center">
+  <div class="container">
+    <div class="menu d-flex justify-content-between align-items-center">
+      <div class="left-menu d-flex">
+        <div class="logo">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Amazon_Prime_Video_logo.svg/1280px-Amazon_Prime_Video_logo.svg.png" alt="">
+        </div>
+        <ul class="nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Store</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Canali</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Categorie</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Area Personale</a>
+          </li>
+        </ul>
+      </div>
+    <div class="right-menu d-flex align-items-center">
+      <i class="fas fa-search "></i>
+      <div class="user mx-2">
+        <i class="fas fa-user "></i>
+      </div>
+      <h6 class="my-0">Salvatore <i class="fas fa-sort-down"></i></h6>
+    </div>
+    </div>
+    
+  </div>
+</header>
